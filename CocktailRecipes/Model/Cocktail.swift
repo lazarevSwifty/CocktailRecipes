64bc3e27efb15deb362f2bfa21bc3e0b72996472
Vlad <vladlazarevdev@mail.ru>
2019-08-31 12:10:10 +0300

    create place
    1. create mainVC and DetailVC
    2. create model

---
+++ CocktailRecipes/Model/Cocktail.swift
@@ -0,0 +1,9 @@
+//
+//  Cocktail.swift
+//  CocktailRecipes
+//
+//  Created by Владислав Лазарев on 31/08/2019.
+//  Copyright © 2019 Владислав Лазарев. All rights reserved.
+//
+
+import Foundation
---
+++ CocktailRecipes/View/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  CocktailRecipes
+//
+//  Created by Владислав Лазарев on 31/08/2019.
+//  Copyright © 2019 Владислав Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
