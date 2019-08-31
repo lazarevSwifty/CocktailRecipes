05c37c23ce384fe46723286359678f0a82aa059d
Vlad <vladlazarevdev@mail.ru>
2019-08-31 11:39:58 +0300

    Initial Commit

---
+++ CocktailRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailRecipes
+//
+//  Created by Владислав Лазарев on 31/08/2019.
+//  Copyright © 2019 Владислав Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
